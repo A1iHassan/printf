e942163699f15e4e854a25b9c2510b1d8e8cad5a
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-17 05:25:54 -0700

    created the put_next_hndl function

---
+++ put_next_hndl.c
@@ -0,0 +1,48 @@
+#include "main.h"
+/**
+ * put_next_hndl - handle speci
+ * @format: format string
+ * @specifier: the speci
+ * buffer: the buffer
+ * Return: the counter
+ */
+int put_next_hndl(va_list specifier, char *format, char *buffer, int counter)
+{
+	switch (*format)
+	{
+		case 'l':
+			switch(*(format + 1))
+			{
+				case 'd':
+					counter = _put_decimal(va_arg(specifier, long), counter, buffer);
+					format++;
+					break;
+				case 'i':
+					counter = _put_i(va_arg(specifier, long), counter, buffer);
+					format++;
+					break;
+				case 'u':
+					counter = _put_unsigned(va_arg(specifier, long), counter, buffer);
+					format++;
+					break;
+				case 'o':
+					counter = _put_octa(va_arg(specifier, long), counter, buffer);
+					format++;
+					break;
+				case 'x':
+					counter = _put_hexa(va_arg(specifier, long), counter, buffer);
+					format++;
+					break;
+				case 'X':
+					counter = _put_hexa_cap(va_arg(specifier, long), counter, buffer);
+					format++;
+					break;
+				default:
+					counter = put_next_hn(specifier, format, buffer, counter);
+					break;
+			}
+
+	}
+	return (counter);
+}
+
