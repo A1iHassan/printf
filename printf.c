d5444d5148ed93621669b865529dacae104ec2c4
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-17 03:47:44 -0700

    created the printf code file

---
+++ printf.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - printf function
+ * @format: format string
+ * @...: args
+ * Return: counter
+*/
+
+int _printf(char *format, ...)
+{
+	va_list specifier;
+	char buff[BUFFER_SIZE];
+	int counter = 0;
+
+	va_start(specifier, format);
+	if (!*format)
+		return (-1);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			counter = deal_with_spec(specifier, (format + 1), buff, counter);
+			format++;
+		}
+		else
+		{
+			counter = _putchar(*format, buff, counter);
+		}
+		format++;
+	}
+	flush(buff, counter);
+	return (counter);
+}
