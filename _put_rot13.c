63c59b6f60ba5c99a1084f2e795c3550d63ecc7d
A1iHassan <ali012wkout@gmail.com>
2023-10-17 08:13:15 -0700

    fixed a bug

--- _put_rot13.c
+++ _put_rot13.c
@@ -8,25 +8,24 @@
 int _put_rot(char *s, char *buff, int counter)
 
 {
-	int i, j;
-	char *rott = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char *ROTT = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *a;
+	char *b;
+	int i = 0;
+	int j;
 
-	i = 0;
+	a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
 	while (s[i] != '\0')
 	{
 		j = 0;
-		while (rott[j] != '\0')
+		while (a[j] != '\0')
 		{
-			if (s[i] == rott[j])
-			{
-				s[i] = ROTT[j];
-				break;
-			}
+			if (s[i] == a[j])
+				counter = _putchar(b[j], buff, counter);
 			j++;
 		}
 		i++;
 	}
-	counter = _puts(s, buff, counter);
 	return (counter);
 }
