36332a4ddf9d1b091a27662dd4fb0cf5ba7609ad
A1iHassan <ali012wkout@gmail.com>
2023-10-17 04:40:17 -0700

    created the put_unsigned function

---
+++ _put_unsigned.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _put_unsigned - print unsigned num
+ * @a: num
+ * @counter: counter
+ * @buf: buffer
+ * Return: counter
+ */
+int _put_unsigned(long a, int counter, char *buf)
+{
+	if (a > 9)
+		counter = _put_decimal(a / 10, counter, buf);
+	counter = _putchar('0' + (a % 10), buf, counter);
+
+	return (counter);
+}
