478af555a62f8fea223886a541b643750ec7f2b3
A1iHassan <ali012wkout@gmail.com>
2023-10-17 04:34:28 -0700

    created the put_hexa function

---
+++ _put_hexa.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _put_hexa - print hexadecimal num
+ * @a: number
+ * @counter: the counter
+ * @buf: buffer
+ * Return: counter
+ */
+int _put_hexa(long a, int counter, char *buf)
+{
+	int i = 0;
+	int j;
+	int arr[64];
+
+	while (a > 0)
+	{
+		arr[i] = a % 16;
+		a /= 16;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		if (arr[j] >= 0 && arr[j] <= 9)
+		{
+			counter = _putchar('0' + arr[j], buf, counter);
+		}
+		else
+		{
+			counter = _putchar(arr[j] + 87, buf, counter);
+		}
+	}
+	return (counter);
+}
