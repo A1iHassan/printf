f6bfd3113275cec7b69f6b5c0f015a5071a39829
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-17 10:28:27 -0700

    a

--- _puts.c
+++ _puts.c
@@ -10,6 +10,11 @@
 
 int _puts(char *str, char *buf, int counter)
 {
+	if (!str)
+	{
+		counter = _puts("(null)", buf, counter);
+		return (counter);
+	}
 	while (*str != '\0')
 	{
 		counter = _putchar(*str, buf, counter);
