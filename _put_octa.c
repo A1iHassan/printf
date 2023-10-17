dce70e9944f63a3dfedb3b9e57983015450dedbe
A1iHassan <ali012wkout@gmail.com>
2023-10-17 04:37:26 -0700

    created the put_octa function

---
+++ _put_octa.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _put_octa - print octal
+ * @a: num
+ * @counter: counter
+ * @buf: buffer
+ * Return: counter
+ */
+int _put_octa(long a, int counter, char *buf)
+{
+	int i = 0;
+	int j;
+	int arr[12];
+
+	while (a > 0)
+	{
+		arr[i] = a % 8;
+		a /= 8;
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		counter = _putchar('0' + arr[j], buf, counter);
+	}
+	return (counter);
+
+}
