4b9f33b3b47b58a0be97237b748a56c2e23393b3
dina3538 <dinaaltayeb1234@gmail.com>
2023-10-17 10:10:32 -0700

    a

--- _put_S.c
+++ _put_S.c
@@ -12,17 +12,8 @@ int _put_S(char *str, int counter, char *buf)
 {
 	int i;
 
-	do {
-		if (*str == '\0' && (*(str + 1) >= 32 && *(str + 1) < 127))
-		{
-			if (*(str + 1) == '%' && *(str + 2) == 'S')
-				break;
-			counter = _putchar('\\', buf, counter);
-			counter = _putchar('x', buf, counter);
-			counter = _putchar('0', buf, counter);
-			counter = _putchar('0', buf, counter);
-			str++;
-		}
+	while (*str)
+	{
 		if (*str >= 32 && *str < 127)
 		{
 			counter = _putchar(*str, buf, counter);
@@ -39,16 +30,6 @@ int _put_S(char *str, int counter, char *buf)
 			}
 		}
 		str++;
-		if (*str == '\0' && (*(str + 1) >= 32 && *(str + 1) < 127))
-		{
-			if (*(str + 1) == '%' && *(str + 2) == 'S')
-				break;
-			counter = _putchar('\\', buf, counter);
-			counter = _putchar('x', buf, counter);
-			counter = _putchar('0', buf, counter);
-			counter = _putchar('0', buf, counter);
-			str++;
-		}
-	} while (*str);
+	}
 	return (counter);
 }
